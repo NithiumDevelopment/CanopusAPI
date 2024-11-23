ccb604d2477f418bcdafbcce580bb265869c4cd7
Calypso <izharat2009@gmail.com>
2024-11-23 18:57:44 +0700

    Forget it.

--- src/main/java/id/nithium/api/canopus/webservice/manager/ServerManager.java
+++ src/main/java/id/nithium/api/canopus/webservice/manager/ServerManager.java
@@ -22,25 +22,14 @@ public class ServerManager {
         }
     }
 
-    public Server getServer(String value) {
-        NithiumHttpResponse<Server> response = api.getNithiumAPI().GET(DataType.DATA_3, "servers/" + value, api.getAPI_KEY(), Server.class);
+    public Server getServer(String name) {
+        NithiumHttpResponse<Server> response = api.getNithiumAPI().GET(DataType.DATA_3, "servers/" + name, api.getAPI_KEY(), Server.class);
 
         if (response.response().getCode() == 200) {
             return response.obj();
         } else {
             if (response.response().getCode() == 404) {
-                try {
-                    int i = Integer.parseInt(value);
-
-                    NithiumHttpResponse<Server> response1 = api.getNithiumAPI().GET(DataType.DATA_3, "servers/" + i, api.getAPI_KEY(), Server.class);
-                    if (response.response().getCode() == 200) {
-                        return response1.obj();
-                    } else {
-                        throw new BadCodeException(api.getDataType(), response1.response().getCode());
-                    }
-                } catch (NumberFormatException e) {
-                    throw new BadCodeException(api.getDataType(), response.response().getCode());
-                }
+                return null;
             } else {
                 throw new BadCodeException(api.getDataType(), response.response().getCode());
             }
